b170ac628fae7b57964c6af88afee18a077801a5
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-10-18 20:25:15 -0400

    commit tree 75812a722f07ec04332d2f83b1b20cd9ae28aef7

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorDiv = document.querySelector('#modal')
+const hearts = document.querySelectorAll('.like-glyph')
+function clickAlert(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then(val=>{
+    
+    if(heart.innerText == EMPTY_HEART){
+      heart.innerText = FULL_HEART
+      heart.classList.add("activated-heart")
+    }
+    else
+    {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    }
+    
+  })
+  .catch(err => {
+    errorDiv.classList.remove("hidden")
+    errorDiv.innerText = err
+    setTimeout(() => errorDiv.classList.add("hidden"), 3000)
+  })
+  }
 
+
+for (const glyph of hearts) {
+  glyph.addEventListener('click', clickAlert);
+
+}
 
 
 
@@ -23,3 +51,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+errorDiv.classList.add("hidden")
+document.addEventListener('DOMContentLoaded', function() {
+})
